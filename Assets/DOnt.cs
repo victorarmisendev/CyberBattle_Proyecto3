b84486069f56d9f1e677fb23889ccd17e71a8967
Victor Armisen <victorarmisencapo@enti.cat>
2019-05-14 18:22:17 +0200

    Beta

---
+++ Assets/DOnt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOnt : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+}
---
+++ Assets/Play.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+    AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        this.audio = GetComponent<AudioSource>();
+        this.audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
